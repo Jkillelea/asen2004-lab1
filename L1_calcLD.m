7fda57fd6a9a7af22aa50ac515a3e367a5f0afa5
Jacob Killelea <Jkillelea@protonmail.ch>
2017-01-31 15:31:38 -0700

    formatting

--- L1_calcLD.m
+++ L1_calcLD.m
@@ -1,7 +1,4 @@
 function [ Lift, Drag ] = L1_calcLD( AOA, N_force, A_force )
-
-Drag = N_force.*sind(AOA) + A_force.*cosd(AOA);
-Lift = N_force.*cosd(AOA) - A_force.*sind(AOA);
-
+  Drag = N_force.*sind(AOA) + A_force.*cosd(AOA);
+  Lift = N_force.*cosd(AOA) - A_force.*sind(AOA);
 end
-
--- L1_main.m
+++ L1_main.m
@@ -1,22 +1,22 @@
-[F16_CLEAN, F16_LOADED, B787, F16_CLEAN_ERR, F16_LOADED_ERR, B787_ERR] = ...
-	L1_readInput('./Data/Group Data Save Here');
+[f16_clean, f16_loaded, b787, f16_clean_err, f16_loaded_err, b787_err] = L1_readInput('./data');
 
-for i = 1:size(F16_CLEAN.P_atm,1);
-	[ F16_CLEAN.L(i,:), F16_CLEAN.D(i,:) ] = ...
-	L1_calcLD(F16_CLEAN.AOA(i), F16_CLEAN.N_force(i), F16_CLEAN.A_force(i));
+for i = 1:size(f16_clean.P_atm,1);
+	[ f16_clean.L(i,:), f16_clean.D(i,:) ] = L1_calcLD(f16_clean.AOA(i), f16_clean.N_force(i), f16_clean.A_force(i));
 end
 
-for i = 1:size(F16_LOADED.P_atm,1);
-	[ F16_LOADED.L(i,:), F16_LOADED.D(i,:) ] = ...
-	L1_calcLD(F16_LOADED.AOA(i), F16_LOADED.N_force(i), F16_LOADED.A_force(i));
+for i = 1:size(f16_loaded.P_atm,1);
+	[ f16_loaded.L(i,:), f16_loaded.D(i,:) ] = L1_calcLD(f16_loaded.AOA(i), f16_loaded.N_force(i), f16_loaded.A_force(i));
 end
 
-for i = 1:size(B787.P_atm,1);
-	[ B787.L(i,:), B787.D(i,:) ] = ...
-	L1_calcLD(B787.AOA(i), B787.N_force(i), B787.A_force(i));
+for i = 1:size(b787.P_atm,1);
+	[ b787.L(i,:), b787.D(i,:) ] = L1_calcLD(b787.AOA(i), b787.N_force(i), b787.A_force(i));
 end
 
-plot(F16_CLEAN.AOA,F16_CLEAN.L,'.','MarkerSize',12)
-xlabel('Angle of Attack, \alpha [^\circ degrees]')
-ylabel('Lift [N]')
-title('F16 Clean Configuration')
+airplane = f16_clean;
+select = airplane.V_air >= 10; % select by airspeed
+data = sortrows([airplane.AOA(select), airplane.L(select)]); % sorts by 1st col, ascending order
+
+plot(data(:, 1), data(:, 2), '.', 'MarkerSize',12);
+xlabel('Angle of Attack, \alpha [^\circ degrees]');
+ylabel('Lift [N]');
+title('F16 Clean Configuration');
