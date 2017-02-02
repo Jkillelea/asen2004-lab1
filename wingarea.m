f216f9e60d42c054654043894f0ec39bdf75af59
Jacob Killelea <Jkillelea@protonmail.ch>
2017-02-02 14:49:53 -0700

    fix scaling (should square scale factor)

--- wingarea.m
+++ wingarea.m
@@ -1,15 +1,15 @@
-function area = wingarea(aircraft)
+function area = wingarea(aircraft_type)
   % calculates the wing area of each model (not full-size)
-  switch aircraft
+  switch aircraft_type
   case 'f16'
     % 300 square ft : http://www.lockheedmartin.com/us/products/f16/F-16Specifications.html
     fullscale = 27.8709;
-    area = fullscale / 48;
+    area = fullscale / (48^2);
     return;
   case 'b787'
     % 3,880 sq ft (360.5 sq m) : https://en.wikipedia.org/wiki/Boeing_787_Dreamliner#Specifications
     fullscale = 360.5;
-    area = fullscale / 225;
+    area = fullscale / (225^2);
     return;
   end
 end
