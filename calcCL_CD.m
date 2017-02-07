5c9878998ec5b336201ee81b0b55e17d836d3c9d
Jacob Killelea <Jkillelea@protonmail.ch>
2017-02-07 15:15:55 -0700

    idk

--- calcCL_CD.m
+++ calcCL_CD.m
@@ -1,6 +1,6 @@
 function [CL, CD] = calcCL_CD(airplane, type)
   area = wingarea(type);
-  [lift, drag] = L1_calcLD(airplane.AOA, airplane.N_force, airplane.A_force);
+  [lift, drag] = L1_calcLD(airplane.AOA, airplane.N_force, airplane.A_force, airplane.q_pitot, area);
 
   CL = lift ./ (airplane.q_pitot .* area);
   CD = drag ./ (airplane.q_pitot .* area);
