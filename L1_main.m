e97c92f786d52813eebbf69ec1bbbacc5942a60e
Jacob Killelea <Jkillelea@protonmail.ch>
2017-02-14 15:18:05 -0700

    Revert "let's see if these merge"
    This reverts commit c457725fd1d828d577e818ff2260b11433e4bae6.

--- L1_main.m
+++ L1_main.m
@@ -1,123 +1,88 @@
+clear all; close all; clc;
+format shortG
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INITIALIZE GIVEN DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[F16_CLEAN, F16_LOADED, B787, F16_CLEAN_ERR, F16_LOADED_ERR, B787_ERR] = L1_readInput('./data');
+mkdir('./graphs');
 
-[F16_CLEAN, F16_LOADED, B787, F16_CLEAN_ERR, F16_LOADED_ERR, B787_ERR] = L1_readInput('./Data/Group Data Save Here');
+F16_CLEAN.S  = wingarea('f16');
+F16_LOADED.S = wingarea('f16');
+B787.S       = wingarea('b787');
 
-[ F16_CLEAN.S, F16_CLEAN.b ]  	= wingarea('f16');
-[ F16_LOADED.S, F16_LOADED.b ]	= wingarea('f16');
-[ B787.S, B787.b ]       		= wingarea('b787');
+F16_CLEAN.c  = 1; % [m]
+F16_LOADED.c = 1; % [m]
+B787.c  	   = 1; % [m]
 
 F16_CLEAN.A  = 14.4*10^-3; % [m]
 F16_LOADED.A = 15.5*10^-3; % [m]
-B787.A  	 = 63.0*10^-3; % [m]
-
-F16_CLEAN.lambda  	= (23.52/79.02)*10^-3; % [m]
-F16_LOADED.lambda 	= (23.52/79.02)*10^-3; % [m]
-B787.lambda  		= (0/53.18)*10^-3; % [m]
-
-F16_CLEAN.c  = (2/3)*(79.02*10^-3)*((F16_CLEAN.lambda^2 + F16_CLEAN.lambda + 1)/(F16_CLEAN.lambda + 1)); % [m]
-F16_LOADED.c = (2/3)*(79.02*10^-3)*((F16_LOADED.lambda^2 + F16_LOADED.lambda + 1)/(F16_LOADED.lambda + 1)); % [m]
-B787.c  	 = (2/3)*(53.18*10^-3)*((B787.lambda^2 + B787.lambda + 1)/(B787.lambda + 1)); % [m]
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CALCULATIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-for i = 1:size(F16_CLEAN.P_atm,1);
-
-	% Lift and drag coefficients
-
-	[ F16_CLEAN.CL(i,:), F16_CLEAN.CD(i,:) ] = L1_calcLD(	F16_CLEAN.AOA(i), ...
-															F16_CLEAN.N_force(i), ...
-															F16_CLEAN.A_force(i), ...
-															F16_CLEAN.q_pitot(i), ...
-															F16_CLEAN.S);
-
-	% Moment coefficient
-
-	[ F16_CLEAN.CM(i,:) ] = L1_calcCM(	F16_CLEAN.M_pitch(i), ...
-										F16_CLEAN.N_force(i), ...
-										F16_CLEAN.A, ...
-										F16_CLEAN.q_pitot(i), ...
-										F16_CLEAN.S, ...
-										F16_CLEAN.c);
-
-end
-
-for i = 1:size(F16_LOADED.P_atm,1);
-
-	% Lift and drag coefficients
-
-	[ F16_LOADED.CL(i,:), F16_LOADED.CD(i,:) ] = L1_calcLD(	F16_LOADED.AOA(i), ...
-															F16_LOADED.N_force(i), ...
-															F16_LOADED.A_force(i), ...
-															F16_LOADED.q_pitot(i), ...
-															F16_LOADED.S);
-
-	% Moment coefficient
-
-	[ F16_LOADED.CM(i,:) ] = L1_calcCM(	F16_LOADED.M_pitch(i), ...
-										F16_LOADED.N_force(i), ...
-										F16_LOADED.A, ...
-										F16_LOADED.q_pitot(i), ...
-										F16_LOADED.S, ...
-										F16_LOADED.c);
-
-
-end
-
-for i = 1:size(B787.P_atm,1);
-
-	% Lift and drag coefficients
-
-	[ B787.CL(i,:), B787.CD(i,:) ] = L1_calcLD(	B787.AOA(i), ...
-												B787.N_force(i), ...
-												B787.A_force(i), ...
-												B787.q_pitot(i), ...
-												B787.S);
-
-	% Moment coefficient
-
-	[ B787.CM(i,:) ] = L1_calcCM(	B787.M_pitch(i), ...
-									B787.N_force(i), ...
-									B787.A, ...
-									B787.q_pitot(i), ...
-									B787.S, ...
-									B787.c);
-
-end
+B787.A  	   = 63.0*10^-3; % [m]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% RE-FORMATTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 airplanes = [{F16_CLEAN}, {F16_LOADED}, {B787}];
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CALCULATIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for i = 1:length(airplanes) % iterate over list of airplanes
+  airplane = airplanes{i};
+
+	for j = 1:size(airplane.P_atm, 1) % calculate CL, CD, and CM
+    [airplane.CL(j, :), airplane.CD(j, :)] = L1_calcLD(airplane.AOA(j), ...
+                                                       airplane.N_force(j), ...
+                                                       airplane.A_force(j), ...
+                                                       airplane.q_pitot(j), ...
+                                                       airplane.S);
+    [ airplane.CM(j, :) ] = L1_calcCM(airplane.M_pitch(j), ...
+                                      airplane.N_force(j), ...
+                                      airplane.A, ...
+                                      airplane.q_pitot(j), ...
+                                      airplane.S, ...
+                                      airplane.c);
+	end
+	airplanes{i} = airplane; % reassign the values back into the list so they make it out of the loop.
+end
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PLOTTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 for i = 1:length(airplanes)
   airplane = airplanes{i};
+	select   = airplane.V_air > 10;
 
 	if sum(size(airplane.P_atm)) > 0
-		figure; % plot CL
-		plot(airplane.AOA, airplane.CL, '.', 'MarkerSize', 12)
+		figure; hold on; % plot CL
+		plot(airplane.AOA(select), airplane.CL(select), '.', 'MarkerSize', 12)
 		xlabel('Angle of Attack, \alpha [^\circ degrees]')
 		ylabel('Lift Coefficient')
-		title(airplane.name)
+		title([airplane.name ' CL vs \alpha'])
 
-		figure; % plot CD
-		plot(airplane.AOA, airplane.CD, '.', 'MarkerSize', 12)
+    print(['./graphs/' airplane.name ' CL vs alpha'],'-djpeg','-noui') % relatives paths are okay since data is on a relative path. Otherwise the data reading failes and nothing is saved
+
+		figure; hold on; % plot CD
+		plot(airplane.AOA(select), airplane.CD(select), '.', 'MarkerSize', 12)
 		xlabel('Angle of Attack, \alpha [^\circ degrees]')
 		ylabel('Drag Coefficient')
-		title(airplane.name)
+		title([airplane.name ' CD vs \alpha'])
 
-		figure; % plot CM
-		plot(airplane.AOA, airplane.CM, '.', 'MarkerSize', 12)
+    print(['./graphs/' airplane.name ' CD vs alpha'],'-djpeg','-noui')
+
+		figure; hold on; % plot CD
+		plot(airplane.CL(select), airplane.CD(select), '.', 'MarkerSize', 12)
+    xlabel('Lift Coefficient');
+    ylabel('Drag Coefficient')
+		title([airplane.name ' Drag Polar'])
+
+    print(['./graphs/' airplane.name ' Drag Polar'],'-djpeg','-noui')
+
+		figure; hold on; % plot CM
+		plot(airplane.AOA(select), airplane.CM(select), '.', 'MarkerSize', 12)
 		xlabel('Angle of Attack, \alpha [^\circ degrees]')
 		ylabel('Moment Coefficient')
-		title(airplane.name)
+		title([airplane.name ' CM vs \alpha'])
 
-		figure; % plot Drag Polar
-		plot(airplane.CL, airplane.CD, '.', 'MarkerSize', 12)
-		xlabel('Lift Coefficient')
-		ylabel('Drag Coefficient')
-		title(airplane.name)
+    print(['./graphs/' airplane.name ' CM vs alpha'],'-djpeg','-noui')
+
+  else
+    disp(['skipping ' airplane.name]);
 	end
 end
-
